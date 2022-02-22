708e5a1560ef520a2806132b0c40c33ce9e503ff
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2022-02-22 08:16:18 -0600

    Reading database configurations from the environment and removed hardcoding

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-engine = create_engine('postgresql+psycopg2://stocksuser:stockspass@stocksdb/stocksdb',
+engine = create_engine(f'postgresql+psycopg2://{os.environ["STOCKS_USER"]}'
+                       f':{os.environ["STOCKS_PASSWORD"]}@{os.environ["STOCKS_DB_HOST"]}/stocksdb',
                        convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
